2525a2d7bcb008964d5f25825cb35b6d9f863db8
student <jessewang@billericak12.com>
2019-10-30 12:16:59 -0400

    Initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public double totalWages(double hrs, double rate){
+        double wages;
+        if (hrs>40)
+        { wages=1.5*rate*hrs; }
+        else
+        { wages=hrs*rate; }
+        return wages;
+    }
+
+    public boolean isLeapYear(int year)
+    {
+        if (year%4=0 && )
+    }
+
+    public isLater()
+    {}
+    public bestMatch()
+    {}
+    public findBestFit()
+    { }
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
